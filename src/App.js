ab4acb9312c85650e69ecb508d98e8fbddbbb1d3
NyoomNyoom <jacksonnorth1275@gmail.com>
2024-06-21 21:58:32 +1200

    Got a BASIC navbar and changed the background colour

--- src/App.js
+++ src/App.js
@@ -4,22 +4,32 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Navbar />
       </header>
+      <body>
+        <HomeBody />
+      </body>
     </div>
   );
+}
+
+function Navbar() {
+  function OnHomeClicked() {
+    console.log("hi")
+  }
+
+  return (
+    <nav>
+      <button onClick={OnHomeClicked}>HOME</button>
+    </nav>
+  )
+}
+
+function HomeBody() {
+  return (
+    <h1>Welcome to my portfolio</h1>
+  )
 }
 
 export default App;
