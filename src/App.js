9d2bc7330b51b87e26525abb4b594df5fef36501
NyoomNyoom <jacksonnorth1275@gmail.com>
2024-06-22 01:45:52 +1200

    Tried to get a SPA working :(
    Might have to resort to a multi page application for the time being

--- src/App.js
+++ src/App.js
@@ -2,34 +2,50 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let bodyContent = <HomeBody />;
+
+  function Navbar() {
+    function OnHomeClicked() {
+      bodyContent = <HomeBody />;
+    }
+
+    function OnPortfolioClicked() {
+      bodyContent = <PortfolioBody />
+    }
+  
+    return (
+      <nav>
+        <button onClick={OnHomeClicked}>HOME</button>
+        <button onClick={OnPortfolioClicked}>PORTFOLIO</button>
+      </nav>
+    )
+  }
+  
+  function HomeBody() {
+    return (
+      <h1>Welcome to my portfolio</h1>
+    )
+  }
+
+  function PortfolioBody() {
+    return (
+      <h1>This is my portfolio</h1>
+    )
+  }
+
   return (
     <div className="App">
       <header>
         <Navbar />
       </header>
       <body>
-        <HomeBody />
+        {bodyContent}
       </body>
+      <footer>
+        <p>Thank you for reading :)</p>
+      </footer>
     </div>
   );
-}
-
-function Navbar() {
-  function OnHomeClicked() {
-    console.log("hi")
-  }
-
-  return (
-    <nav>
-      <button onClick={OnHomeClicked}>HOME</button>
-    </nav>
-  )
-}
-
-function HomeBody() {
-  return (
-    <h1>Welcome to my portfolio</h1>
-  )
 }
 
 export default App;
