1b87aaf0508a2d18e296fab61d7d61cbbe4cd553
NyoomNyoom <jacksonnorth1275@gmail.com>
2024-06-24 20:05:26 +1200

    Added a comment re: how I may get SPA working
    The method could be quite janky tho

--- src/App.js
+++ src/App.js
@@ -8,9 +8,10 @@ export default function App() {
   return (
     <>
       <HomePage />
-      <footer>
-        
-      </footer>
+      <About />
     </>
   )
 }
+
+// Use the parameters for App() to pass through what page I want rendered when it is
+// called from index.js. Just have to work out how this my work.
