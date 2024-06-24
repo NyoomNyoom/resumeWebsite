1da33aa2872729ab70c70f3281015c5cace81ce6
NyoomNyoom <jacksonnorth1275@gmail.com>
2024-06-24 18:50:17 +1200

    Fixed the compile time errors I was getting

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
 
-import {Route, Switch} from 'react-router-dom';
 
 import About from './pages/About';
 import HomePage from './pages/HomePage';
 
 export default function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/:id" component ={About} />
-    </Switch>
+    <>
+      <HomePage />
+      <footer>
+        
+      </footer>
+    </>
   )
 }
