d3f2cadc979851feb7992572e0c53f53e46f16aa
NyoomNyoom <jacksonnorth1275@gmail.com>
2024-06-24 23:34:14 +1200

    Added a comment for my footer

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -3,6 +3,7 @@ import React from 'react'
 export default function Footer() {
     return(
         <div className="footer">
+            {/* I have used rel="norefferer" here to cover a vulnerability that was reported while I was using target to open a new tab. */}
             <p>To see the development of this resume wesbite please visit my <a href='https://www.github.com/nyoomnyoom/resumeWebsite' target="_blank" rel="noreferrer">github repository</a>.</p>
         </div>
     )
