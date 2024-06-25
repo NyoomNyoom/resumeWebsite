16739763367e39b41b86dd1ed2d9afd4fe39fe3f
NyoomNyoom <jacksonnorth1275@gmail.com>
2024-06-25 13:55:38 +1200

    Added some code for my portfolio

---
+++ src/pages/Portfolio.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Portfolio() {
+    return(
+        <div className='Portfolio'>
+            <h1>Welcome to my portfolio</h1>
+            <p>These are my cool projects</p>
+        </div>
+    )
+}
