69fcfeee5114a286495559a5bc32c783f681ef00
NyoomNyoom <jacksonnorth1275@gmail.com>
2024-06-25 20:24:55 +1200

    Created a page for my shell app that I developed

---
+++ src/pages/ShellApp.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function ShellApp() {
+    return (
+        <>
+            <h1>Shell App</h1>
+            <p>A C++ program that I made to replicate a shell/terminal.</p>
+        </>
+    )
+}
