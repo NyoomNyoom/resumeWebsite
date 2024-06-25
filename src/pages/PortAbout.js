87900c3bb2c12632176742c8333dec0dd63b8a19
NyoomNyoom <jacksonnorth1275@gmail.com>
2024-06-25 20:24:38 +1200

    Created an about page for my portfolio

---
+++ src/pages/PortAbout.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function PortAbout() {
+    return (
+        <>
+            <h1>My Portfolio</h1>
+            <p>Please click one of the buttons at the top to select a project for more information</p>
+        </>
+    )
+}
