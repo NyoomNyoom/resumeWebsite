59348a3e12fdb7b413c414d8a1a8a7e68d06e0d1
NyoomNyoom <jacksonnorth1275@gmail.com>
2024-06-26 22:08:50 +1200

    Added some actual content to one of my portfolio pages

--- src/pages/ShellApp.js
+++ src/pages/ShellApp.js
@@ -2,9 +2,18 @@ import React from "react"
 
 export default function ShellApp() {
     return (
-        <div className="portfolio shellApp">
-            <h1>Shell App</h1>
-            <p>A C++ program that I made to replicate a shell/terminal.</p>
+        <div className="shellApp">
+            <h1 className="portfolioTitle">Shell Clone</h1>
+            <h2>About</h2>
+            <p>This project is a clone of the command prompt. I developed the first few modules using guidance from a free course on Code Crafters. How ever, it was up to me to chose
+                the language that I coded in and if I wanted to continue this further. I chose C++ to code this in as it was a langauge I knew little about and wanted to learn more about.
+            </p>
+            <p>
+                The project can be found on <a href="https://github.com/NyoomNyoom/NyoomNyoom-CC-shell-app" target="_blank" rel="noreferrer">github</a>. Please feel free to view this and follow the steps in the README file on how to
+                clone and run this project your self!
+            </p>
+            <h2>Preview</h2>
+            <p>Below I have added some pictures of the program running to highlight some of the implemented features that I am most proud of.</p>
         </div>
     )
 }
